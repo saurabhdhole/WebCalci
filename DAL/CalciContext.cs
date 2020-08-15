0647c18c77c347676a2a00b6b102df61b191fddd
Saurabh Dhole <saurabh.dhole@rocketmail.com>
2020-08-15 18:00:34 -0400

    first commit

---
+++ DAL/CalciContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using WebCalci.Models;
+
+namespace WebCalci.DAL
+{
+    public class CalciContext : DbContext
+    {
+
+        public CalciContext() : base("CalciContext")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<WebCalci.Models.User> Users { get; set; }
+    }
+}
---
+++ Scripts/main.js
@@ -0,0 +1,66 @@
+﻿var previous=0, current=0, op="";
+var res = 0;
+
+function setClear() {
+    this.current = "";
+    this.previous = "";
+    this.op = "";
+    document.getElementById("calRes").innerText = "";
+}
+
+function setCurrent(curr) {
+    if (op == "=") {
+        document.getElementById("calRes").innerText = "";
+        op = "";
+    }
+    document.getElementById("calRes").innerText = document.getElementById("calRes").innerText + curr.innerText;
+    this.current = document.getElementById("calRes").innerText;
+}
+
+function setPrevios() {
+    this.previous = this.current;
+}
+
+function setOperator(op) {
+
+    if (op.innerText == "=") {
+        performOperation(this.op);
+        //set result to screen
+        document.getElementById("calRes").innerText = res;
+        //setCurrent(res);
+        this.current = res;
+        this.op = op.innerText;
+    }
+    else {
+        this.op = op.innerText;
+        
+
+        setPrevios();
+        document.getElementById("calRes").innerText = "";
+
+    }
+    
+   
+}
+
+
+function performOperation(op) {
+    
+    switch (op) {
+        case "+": res = parseFloat(this.previous) + parseFloat(this.current);
+            break;
+
+        case "-": res = parseFloat(this.previous) - parseFloat(this.current);
+            break;
+
+        case "/": res = parseFloat(this.previous) / parseFloat(this.current);
+            break;
+
+        case "*": res = parseFloat(this.previous) * parseFloat(this.current);
+            break;
+        case "%": res = parseFloat(this.current) / 100;
+            break;
+        case "f2c": res = (parseFloat(this.current) -32) *5/9;
+            break;
+    }
+}
